79554572a166cb17aa456f3fd5994c1c217197f2
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2025-08-26 09:17:35 +0630

    MODIFIED: backend feature

--- backend/src/routes/teacherRoutes.ts
+++ backend/src/routes/teacherRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import {  getOfferingCoursesByTeacherId, getAllTeachers } from '../controllers/userController';
-import { authenticateToken, requireAdmin, requireAdminOrTeacher } from '../middleware/auth';
+import {  getOfferingCoursesByTeacherId, getAllTeachers, getCourseChatRoomsByTeacherId } from '../controllers/userController';
+import { authenticateToken, requireAdmin, requireAdminOrSelf, requireAdminOrTeacher } from '../middleware/auth';
 
 const router = Router();
 
 router.get('/', authenticateToken, requireAdmin, getAllTeachers);
 router.get('/:teacherId/course-offerings', authenticateToken, requireAdminOrTeacher, getOfferingCoursesByTeacherId);
-
+router.get('/:teacherId/course-chat-rooms', authenticateToken, requireAdminOrSelf, getCourseChatRoomsByTeacherId);
 export default router;
