bbf5566df3f3dbf753d7676f20e27b043ae94a9f
Aung Khant Kyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2025-08-26 09:22:20 +0630

    MODIFIED: backend feature (#14)
    * FIX: Database Structure & ADD: Backend API
    
    * FIX: Database Structure & ADD: Backend API (#5) (#6)
    
    * UPDATE: akk branch (#8)
    
    * FIX: Database Structure & ADD: Backend API (#5)
    
    * AKK branch  (#7)
    
    * FIX: Database Structure & ADD: Backend API
    
    * FIX: Database Structure & ADD: Backend API (#5) (#6)
    
    * ADD: New Feature
    
    * ADD
    
    * ADD: API route & MODIFY: route and controller
    
    * UPDATE: API routes and controllers for chat rooms, courses, majors, students, and teachers
    
    * MODIFIED: backend feature

--- backend/src/controllers/chatRoomCongroller.ts
+++ backend/src/controllers/chatRoomCongroller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from 'express';
 import { db } from '../db';
-import { academicChatRooms, courseChatRooms } from './../db/schema';
-import { eq } from "drizzle-orm";
+import { academicChatRooms, chatMembers, courseChatRooms } from './../db/schema';
+import { and, eq } from "drizzle-orm";
 
 export const getAllAcademicChatRooms = async (req: Request, res: Response) => {
   try {
@@ -53,6 +53,46 @@ export const getCourseChatRoomById = async (req: Request, res: Response) => {
         return res.status(200).json({ status: "success", data: chatRoom })
     } catch (error) {
         console.error("Error fetching academic chat rooms:", error);
+        return res.status(500).json({ status: "error", message: "Internal Server Error" });
+    }
+};
+
+export const getAcademicChatRoomMembers = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    
+    try {
+      const memberList = await db.select().from(chatMembers).where(and(
+        eq(chatMembers.roomId, id),
+        eq(chatMembers.roomType, 'ACADEMIC')
+      ));
+
+      if (memberList.length === 0) {
+          return res.status(404).json({ status: "error", message: "No members found for this chat room" });
+      }
+
+      return res.status(200).json({ status: "success", data: memberList, count: memberList.length })
+    } catch (error) {
+        console.error("Error fetching chat room members:", error);
+        return res.status(500).json({ status: "error", message: "Internal Server Error" });
+    }
+};
+
+export const getCourseChatRoomMembers = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const memberList = await db.select().from(chatMembers).where(and(
+            eq(chatMembers.roomId, id),
+            eq(chatMembers.roomType, 'COURSE')
+        ));
+
+        if (memberList.length === 0) {
+            return res.status(404).json({ status: "error", message: "No members found for this chat room" });
+        }
+
+        return res.status(200).json({ status: "success", data: memberList, count: memberList.length })
+    } catch (error) {
+        console.error("Error fetching chat room members:", error);
         return res.status(500).json({ status: "error", message: "Internal Server Error" });
     }
 };
--- backend/src/routes/chatRoomRoutes.ts
+++ backend/src/routes/chatRoomRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAcademicChatRoomById, getAllAcademicChatRooms, getAllCourseChatRooms, getCourseChatRoomById } from "../controllers/chatRoomCongroller";
+import { getAcademicChatRoomById, getAcademicChatRoomMembers, getAllAcademicChatRooms, getAllCourseChatRooms, getCourseChatRoomById, getCourseChatRoomMembers } from "../controllers/chatRoomCongroller";
 import { authenticateToken } from "../middleware/auth";
 
 const router = Router();
@@ -8,8 +8,11 @@ router.get("/", authenticateToken, (req, res) => {
     res.send("Chat Room");
 });
 router.get("/academic", authenticateToken, getAllAcademicChatRooms);
-router.get("/course", authenticateToken, getAllCourseChatRooms);
 router.get("/academic/:id", authenticateToken, getAcademicChatRoomById);
+router.get("/academic/:id/members", authenticateToken, getAcademicChatRoomMembers);
+
+router.get("/course", authenticateToken, getAllCourseChatRooms);
 router.get("/course/:id", authenticateToken, getCourseChatRoomById);
+router.get("/course/:id/members", authenticateToken, getCourseChatRoomMembers);
 
 export default router;
